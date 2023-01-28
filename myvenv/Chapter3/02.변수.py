cb0d7c2682959c147632582fd3b28a81e458ccc4
Jichang Jung <jichangjung@yahoo.com>
2023-01-28 18:50:22 +0900

    first python

---
+++ myvenv/Chapter3/02.변수.py
@@ -0,0 +1,17 @@
+# 변수
+# 변수이름 = 데이터
+
+# 마스터이 챔피언 데이털르 변수에 저장
+name = "마스터이"
+level = 5
+health = 800
+attack = 90
+
+print(name, level, health, attack, sep=' and ')
+
+# 변수에 저장된 데이터 변경
+level = level + 1 # 5 + 1
+health = health + 100 # 800 + 100
+attack = 100
+
+print(name, level, health, attack, sep=' and ') 
