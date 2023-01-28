7262f829962ccdfb54f3a3a72387e90434a7172a
Jichang Jung <jichangjung@yahoo.com>
2023-01-28 19:43:37 +0900

    논리 연산

--- myvenv/Chapter4/연산_two.py
+++ myvenv/Chapter4/연산_two.py
@@ -6,3 +6,11 @@ print(3 <= 3) # True
 print("팕팕팔" == "팕팕팕") # False
 print("11111111111" != "1111111111") # True
 
+print("논리 연산")
+# 2. 논리연산
+print(4 < 6 and 10 >= 10) # True
+print("포기하지말아요" != "포기하지말아요" or "나는 할 수 있다" == "나는 할 수 있다") # True
+print(not 5==5) # False
+
+print("멤버십 연산")
+# 3. 멤버십 연산
