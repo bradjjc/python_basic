0486ad8606c80efc5b4744f3e5ee5a2834c2bc8b
Jichang Jung <jichangjung@yahoo.com>
2023-01-28 19:06:11 +0900

    숫자연산

---
+++ myvenv/Chapter4/연산_one.py
@@ -0,0 +1,17 @@
+# 1. 대입연산
+# 변수이름 = 데이터
+
+# 2. 산술연산
+# - 숫자연산
+x = 5
+y = 2
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y) # 몫
+print(x % y) # 나머지
+print(x ** y) # 제곱
+
+# - 문자열 연산
